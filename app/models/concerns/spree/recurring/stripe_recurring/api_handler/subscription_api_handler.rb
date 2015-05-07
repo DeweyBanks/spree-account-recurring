0689af217d1b19cca20ff2bee0373aa91a5d82a0
Don Livanec <donlivanec@gmail.com>
2015-05-07 16:43:02 -0400

    feat: multiple calls to stripe to update quantity and tax_rates

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
@@ -6,7 +6,11 @@ module Spree
           def subscribe(subscription)
             raise_invalid_object_error(subscription, Spree::Subscription)
             customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
-            customer.subscriptions.create(plan: subscription.api_plan_id, trial_end:subscription.trial_end)
+            stripe_subscription = customer.subscriptions.create(plan: subscription.api_plan_id, trial_end:subscription.trial_end)
+            #TODO: merge second stripe call into first, the combined call works in test and pry, but not in live code
+            stripe_subscription.quantity = subscription.quantity            
+            stripe_subscription.tax_percent = subscription.tax_percent
+            stripe_subscription.save
           end
 
           def unsubscribe(subscription)
